b14f4b5a4dfda395998aa022c77752d795fb61e1
WeddedMercury15 <wocaowuqing@yandex.com>
2023-07-28 14:34:52 +0800

    Update

--- main.py
+++ main.py
@@ -1,45 +1,48 @@
-import curses
+from asciimatics.widgets import Frame, ListBox, Layout, Divider, Text, \
+    Button, TextBox, Widget
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+from asciimatics.exceptions import ResizeScreenError, StopApplication
+import sys
 
-def main(stdscr):
-    curses.curs_set(0)
-    stdscr.keypad(1)
-    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+class DemoFrame(Frame):
+    def __init__(self, screen):
+        super(DemoFrame, self).__init__(screen,
+                                        screen.height,
+                                        screen.width,
+                                        on_load=self._reload_list,
+                                        hover_focus=True,
+                                        can_scroll=False,
+                                        title="Pingoo 图形化界面 v1.0.0")
+        layout = Layout([100], fill_frame=True)
+        self.add_layout(layout)
+        self._list = ListBox(
+            Widget.FILL_FRAME,
+            [("TCP", 1), ("UDP", 2), ("ICMP", 3)],
+            name="protocols",
+            on_change=self._on_pick)
+        layout.add_widget(self._list)
+        layout2 = Layout([1, 1, 1, 1])
+        self.add_layout(layout2)
+        layout2.add_widget(Button("Quit", self._quit), 3)
+        self.fix()
 
-    current_row = 0
+    def _on_pick(self):
+        pass
 
-    while True:
-        stdscr.clear()
-        h, w = stdscr.getmaxyx()
-        box = stdscr.subwin(h-2, w-2, 1, 1)
-        box.box()
-        title = "Pingoo 图形化界面 v1.0.0"
-        start_x_title = int((w // 2) - (len(title) // 2) - len(title) % 2)
-        box.addstr(0, start_x_title, title)
-        box.addstr(2, 3, "1. TCP")
-        box.addstr(3, 3, "2. UDP")
-        box.addstr(4, 3, "3. ICMP")
-        if current_row == 0:
-            box.attron(curses.color_pair(1))
-            box.addstr(2, 3, "1. TCP")
-            box.attroff(curses.color_pair(1))
-        elif current_row == 1:
-            box.attron(curses.color_pair(1))
-            box.addstr(3, 3, "2. UDP")
-            box.attroff(curses.color_pair(1))
-        elif current_row == 2:
-            box.attron(curses.color_pair(1))
-            box.addstr(4, 3, "3. ICMP")
-            box.attroff(curses.color_pair(1))
+    def _reload_list(self):
+        pass
 
-        stdscr.refresh()
-        key = stdscr.getch()
+    @staticmethod
+    def _quit():
+        raise StopApplication("User pressed quit")
 
-        if key == curses.KEY_UP and current_row > 0:
-            current_row -= 1
-        elif key == curses.KEY_DOWN and current_row < 2:
-            current_row += 1
-        elif key == ord('\t'):
-            # 切换到其他框
-            pass
+def demo(screen, scene):
+    screen.play([Scene([DemoFrame(screen)], -1)], stop_on_resize=True)
 
-curses.wrapper(main)
+while True:
+    try:
+        Screen.wrapper(demo)
+        sys.exit(0)
+    except ResizeScreenError:
+        pass
