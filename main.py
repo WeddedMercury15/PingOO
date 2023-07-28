aa6fe495e6867099af3be8578fb5dc33a577a666
WeddedMercury15 <wocaowuqing@yandex.com>
2023-07-28 14:26:01 +0800

    Update

--- main.py
+++ main.py
@@ -1,53 +1,45 @@
-import os
-import keyboard
+import curses
 
-def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
+def main(stdscr):
+    curses.curs_set(0)
+    stdscr.keypad(1)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
-def handle_key_event(event):
-    global current_option
-    key = event.name
-
-    if key == 'up':  # 上箭头
-        current_option = (current_option - 1) % len(options)
-    elif key == 'down':  # 下箭头
-        current_option = (current_option + 1) % len(options)
-    elif key == 'enter':  # 回车键
-        selected_option = options[current_option]
-        if selected_option == "TCPing":
-            os.system("python tools/tcping.py")
-        elif selected_option == "UDPing":
-            os.system("python tools/udping.py")
-        elif selected_option == "ICMPing":
-            os.system("python tools/icmping.py")
-    elif key == 'esc':  # ESC键
-        keyboard.unhook_all()
-        return False
-
-    return True
-
-def main():
-    global options, current_option
-    options = ["TCPing", "UDPing", "ICMPing"]
-    current_option = 0
-
-    keyboard.on_press_key('up', handle_key_event)
-    keyboard.on_press_key('down', handle_key_event)
-    keyboard.on_press_key('enter', handle_key_event)
-    keyboard.on_press_key('esc', handle_key_event)
+    current_row = 0
 
     while True:
-        clear_screen()
+        stdscr.clear()
+        h, w = stdscr.getmaxyx()
+        box = stdscr.subwin(h-2, w-2, 1, 1)
+        box.box()
+        title = "Pingoo 图形化界面 v1.0.0"
+        start_x_title = int((w // 2) - (len(title) // 2) - len(title) % 2)
+        box.addstr(0, start_x_title, title)
+        box.addstr(2, 3, "1. TCP")
+        box.addstr(3, 3, "2. UDP")
+        box.addstr(4, 3, "3. ICMP")
+        if current_row == 0:
+            box.attron(curses.color_pair(1))
+            box.addstr(2, 3, "1. TCP")
+            box.attroff(curses.color_pair(1))
+        elif current_row == 1:
+            box.attron(curses.color_pair(1))
+            box.addstr(3, 3, "2. UDP")
+            box.attroff(curses.color_pair(1))
+        elif current_row == 2:
+            box.attron(curses.color_pair(1))
+            box.addstr(4, 3, "3. ICMP")
+            box.attroff(curses.color_pair(1))
 
-        # 打印选项列表
-        for i, option in enumerate(options):
-            if i == current_option:
-                print(f"> {option}")
-            else:
-                print(f"  {option}")
+        stdscr.refresh()
+        key = stdscr.getch()
 
-        # 通过捕获按键事件并设置block参数为False来持续处理按键事件
-        keyboard.read_event(suppress=True)
+        if key == curses.KEY_UP and current_row > 0:
+            current_row -= 1
+        elif key == curses.KEY_DOWN and current_row < 2:
+            current_row += 1
+        elif key == ord('\t'):
+            # 切换到其他框
+            pass
 
-if __name__ == "__main__":
-    main()
+curses.wrapper(main)
