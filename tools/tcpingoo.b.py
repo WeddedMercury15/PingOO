67d845f318494f109678ff6c0fc91234a7eb120c
WeddedMercury15 <wocaowuqing@yandex.com>
2023-07-29 22:02:01 +0800

    If the system allows ipv4, then the system’s ipv4 dns server is given priority for querying, and if there are multiple dns results (IPV4 or IPV6), take one of the ipv4DNS results for tcping, and if it cannot be accessed, take an IPV6DNS record result for tcping Inquire. If the system allows ipv6, and ipv4 is not allowed, then the ipv6 dns server of the system is preferred for query, and if there are multiple dns results (IPV4 or IPV6), take one of the ipv6 DNS results for tcping. If the system allows ipv4 and ipv6 at the same time, then query according to the above rules that the system allows ipv4, otherwise try IPV6 query until a DNS result can be resolved normally. The above content has nothing to do with the two parameters -4 -6 in the script.
    If the -4 parameter is used, only IPV4 is used for DNS query and only one IPV4DNS result in the result is used for tcping. If the -6 parameter is used, only IPV4 is used for DNS query and only one IPV6DNS result in the result is used for tcping. If you use the two parameters -4 -6, the above is the requirement.

--- tools/tcpingoo.b.py
+++ tools/tcpingoo.b.py
@@ -2,7 +2,6 @@ import sys
 import socket
 import time
 import argparse
-import dns.resolver
 
 def custom_gaierror(msg):
     class CustomGaiError(socket.gaierror):
@@ -11,51 +10,43 @@ def custom_gaierror(msg):
 
     raise CustomGaiError(msg)
 
-def resolve_ip(hostname, dns_server=None, force_ipv4=False):
+def resolve_ip(hostname, force_ipv4=False):
     try:
-        if dns_server:
-            resolver = dns.resolver.Resolver()
-            resolver.nameservers = [dns_server]
-            answer = resolver.query(hostname, "A" if force_ipv4 else "AAAA", raise_on_no_answer=False)
-            if answer.rrset is not None:
-                ip = str(answer.rrset[0])
-            else:
-                raise TimeoutError("DNS query to custom server timed out.")
-        else:
-            family = socket.AF_INET6 if not force_ipv4 else socket.AF_INET
-            addr_info = socket.getaddrinfo(hostname, None, family, socket.SOCK_STREAM, 0, socket.AI_ADDRCONFIG)
-            ip = addr_info[0][4][0]  # Get the IP address
+        # 默认使用IPv4进行DNS查询
+        family = socket.AF_INET if force_ipv4 else socket.AF_INET6
+
+        # 使用系统默认的DNS服务器进行查询
+        addr_info = socket.getaddrinfo(hostname, None, family)
+        ip = addr_info[0][4][0]  # 获取IP地址
 
         return ip
 
     except socket.gaierror:
         raise ValueError(f"TCPing 请求找不到主机 {hostname}。请检查该名称，然后重试.")
 
-def tcping(domain, port, request_nums, force_ipv4, force_ipv6, dns_server=None, timeout=1000):
+def tcping(domain, port, request_nums, force_ipv4, force_ipv6, timeout=1000):
     try:
         ip = None
 
-        if dns_server:
-            try:
-                # Check if the custom DNS server is valid and reachable
-                resolve_ip(domain, dns_server, force_ipv4=False)
-                # If valid, resolve the domain using the custom DNS server
-                ip = resolve_ip(domain, dns_server, force_ipv4=False)
-            except ValueError:
-                raise ValueError(f"TCPing 请求找不到主机 {domain}。请检查该名称，然后重试。")
-
-        elif force_ipv4:
-            ip = resolve_ip(domain, dns_server, force_ipv4=True)
+        if force_ipv4:
+            # 如果使用 -4 参数，只使用IPv4进行DNS查询
+            ip = resolve_ip(domain, force_ipv4=True)
         elif force_ipv6:
-            ip = resolve_ip(domain, dns_server, force_ipv4=False)
+            # 如果使用 -6 参数，只使用IPv6进行DNS查询
+            ip = resolve_ip(domain, force_ipv4=False)
         else:
-            # Resolve using IPv4 as default
-            ip = resolve_ip(domain, dns_server, force_ipv4=True)
+            # 否则，根据系统的网络配置来选择DNS查询方式
+            try:
+                # 尝试使用IPv4进行DNS查询
+                ip = resolve_ip(domain, force_ipv4=True)
+            except ValueError:
+                # 如果IPv4查询失败，则使用IPv6进行DNS查询
+                ip = resolve_ip(domain, force_ipv4=False)
 
         print(f"\n正在 TCPing {domain}:{port} [{ip}:{port}] 具有 32 字节的数据:")
         received_count = 0
         response_times = []
-        total_sent = 0  # Initialize total_sent to track the actual number of packets sent
+        total_sent = 0  # 初始化total_sent以跟踪实际发送的数据包数量
 
         try:
             while total_sent < request_nums:
@@ -67,7 +58,7 @@ def tcping(domain, port, request_nums, force_ipv4, force_ipv6, dns_server=None, 
                         received_count += 1
                         response_times.append(response_time)
                         print(f"来自 {ip}:{port} 的回复: 字节=32 时间={response_time:.0f}ms TTL=64")
-                        total_sent += 1  # Increment total_sent only when a packet is sent
+                        total_sent += 1  # 仅当发送数据包时才增加total_sent
                         time.sleep(1)
                 except socket.timeout:
                     print("请求超时。")
@@ -78,7 +69,7 @@ def tcping(domain, port, request_nums, force_ipv4, force_ipv6, dns_server=None, 
                         time.sleep(1)
                     else:
                         print(f"无法连接到 {ip}:{port}。")
-                        total_sent += 1  # Increment total_sent even if the packet is lost
+                        total_sent += 1  # I即使数据包丢失，也会增加total_sent
                         time.sleep(1)
 
         except KeyboardInterrupt:
@@ -110,31 +101,51 @@ def print_help():
     -n count       计划发送的请求数 (默认: 4)。
     -d DNS_server  自定义 DNS 服务器地址。
     -w timeout     每次请求的超时时间(毫秒) (默认: 1000)。
-    -4             强制使用 IPv4。
-    -6             强制使用 IPv6。
-    """)
+    -4             强制使用 IPv4 进行查询。
+    -6             强制使用 IPv6 进行查询。
+
+示例:
+    python tcpingoo.b.py yohoky.com 80
+    python tcpingoo.b.py yohoky.com 80 -d 1.1.1.1
+    python tcpingoo.b.py yohoky.com 80 -n 10 -w 500
+    python tcpingoo.b.py yohoky.com 80 -4
+    python tcpingoo.b.py yohoky.com 80 -6
+""")
+    sys.exit(0)
 
 def main():
-    # 使用argparse模块处理命令行参数
-    parser = argparse.ArgumentParser(description="TCPing - Test TCP connectivity to a host and port.")
-    parser.add_argument("domain", type=str, help="Target domain or IP address to TCPing.")
-    parser.add_argument("port", type=int, help="Target port number to TCPing.")
-    parser.add_argument("-n", dest="request_nums", type=int, default=4, help="Number of requests to send (default: 4).")
-    parser.add_argument("-d", dest="dns_server", type=str, help="Custom DNS server address for resolution.")
-    parser.add_argument("-w", dest="timeout", type=int, default=1000, help="Timeout for each request in milliseconds (default: 1000).")
+    parser = argparse.ArgumentParser(description="TCPing 工具 - 使用 TCP 协议检查目标主机端口的可达性。",
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog="示例:\n"
+                                            "python tcpingoo.b.py yohoky.com 80\n"
+                                            "python tcpingoo.b.py yohoky.com 80 -d 1.1.1.1\n"
+                                            "python tcpingoo.b.py yohoky.com 80 -n 10 -w 500\n"
+                                            "python tcpingoo.b.py yohoky.com 80 -4\n"
+                                            "python tcpingoo.b.py yohoky.com 80 -6")
 
-    # 通过添加互斥组，确保 -4 和 -6 参数互斥
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-4", dest="force_ipv4", action="store_true", help="Force using IPv4.")
-    group.add_argument("-6", dest="force_ipv6", action="store_true", help="Force using IPv6.")
+    parser.add_argument("domain", help="要 TCPing 的目标主机名。")
+    parser.add_argument("port", type=int, help="目标主机的端口号。")
+    parser.add_argument("-n", dest="request_nums", metavar="count", type=int, default=4,
+                        help="计划发送的请求数 (默认: 4)。")
+    parser.add_argument("-d", dest="dns_server", metavar="DNS_server", default=None,
+                        help="自定义 DNS 服务器地址。")
+    parser.add_argument("-w", dest="timeout", metavar="timeout", type=int, default=1000,
+                        help="每次请求的超时时间(毫秒) (默认: 1000)。")
+    parser.add_argument("-4", dest="force_ipv4", action="store_true",
+                        help="强制使用 IPv4 进行查询。")
+    parser.add_argument("-6", dest="force_ipv6", action="store_true",
+                        help="强制使用 IPv6 进行查询。")
+
+    if len(sys.argv) == 1:
+        print_help()
 
     args = parser.parse_args()
 
     try:
         if args.request_nums < 1:
-            args.request_nums = 4  # Set default value to 4 if no value is specified
+            args.request_nums = 4  # 如果未指定值，则将默认值设置为 4
 
-        tcping(args.domain, args.port, args.request_nums, args.force_ipv4, args.force_ipv6, args.dns_server, args.timeout)
+        tcping(args.domain, args.port, args.request_nums, args.force_ipv4, args.force_ipv6, args.timeout)
     except ValueError as e:
         print(e)
 
