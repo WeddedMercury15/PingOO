6a40404c474d7f32126b0cb7bfadf985d190ac3a
WeddedMercury15 <wocaowuqing@yandex.com>
2023-07-30 07:52:26 +0800

    Copy

--- tools/tcpingoo.py
+++ tools/tcpingoo.py
@@ -1,10 +1,11 @@
-# -*- coding: utf-8 -*-
-
 import sys
 import socket
-import subprocess
 import time
 import argparse
+import signal
+
+# Add this global flag variable to track whether Ctrl+C was used or not
+ctrl_c_used = False
 
 def custom_gaierror(msg):
     class CustomGaiError(socket.gaierror):
@@ -13,227 +14,227 @@ def custom_gaierror(msg):
 
     raise CustomGaiError(msg)
 
-def resolve_ip(hostname, dns_server=None, force_ipv4=False):
-    try:
-        if dns_server:
-            resolver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            resolver.settimeout(1)
-            resolver.connect((dns_server, 53))
-            addr_info = socket.getaddrinfo(hostname, None, socket.AF_INET6)
-            ip = addr_info[0][4][0]  # 获取 IPv6 地址
-            resolver.close()
-        elif force_ipv4:
-            # 使用socket.getaddrinfo进行DNS解析，支持IPv4
-            addr_info = socket.getaddrinfo(hostname, None, socket.AF_INET)
-            ip = addr_info[0][4][0]  # 获取 IPv4 地址
-        else:
-            # 尝试使用 IPv4 解决
-            try:
-                addr_info = socket.getaddrinfo(hostname, None, socket.AF_INET)
-                ip = addr_info[0][4][0]  # 获取 IPv4 地址
-            except socket.gaierror:
-                # 如果 IPv4 解析失败，请尝试使用 IPv6
-                addr_info = socket.getaddrinfo(hostname, None, socket.AF_INET6)
-                ip = addr_info[0][4][0]  # 获取 IPv6 地址
-        return ip
-    except socket.gaierror:
-        raise ValueError(f"TCPing 请求找不到主机 {hostname}。请检查该名称，然后重试。")
+def signal_handler(sig, frame):
+    global ctrl_c_used
+    ctrl_c_used = True
 
-def tcping(domain, port, request_nums, force_ipv4, force_ipv6, dns_server=None, timeout=1000, continuous_ping=False):
+def resolve_ip(hostname, force_ipv4=False):
+    try:
+        # 默认使用IPv4进行DNS查询
+        family = socket.AF_INET if force_ipv4 else socket.AF_INET6
+
+        # 使用系统默认的DNS服务器进行查询
+        addr_info = socket.getaddrinfo(hostname, None, family)
+        ip = addr_info[0][4][0]  # 获取IP地址
+
+        return ip
+
+    except socket.gaierror:
+        raise ValueError(f"TCPing 请求找不到主机 {hostname}。请检查该名称，然后重试.")
+
+def tcping(domain, port, request_nums, force_ipv4, force_ipv6, timeout=1000):
     try:
         ip = None
 
-        if dns_server:
-            resolver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            resolver.settimeout(1)
-            resolver.connect((dns_server, 53))
-            addr_info = socket.getaddrinfo(domain, None, socket.AF_INET6)
-            ip = addr_info[0][4][0]  # 获取 IPv6 地址
-            resolver.close()
-        elif force_ipv4:
-            ip = resolve_ip(domain, dns_server, force_ipv4=True)
+        if force_ipv4:
+            # 如果使用 -4 参数，只使用IPv4进行DNS查询
+            ip = resolve_ip(domain, force_ipv4=True)
         elif force_ipv6:
-            ip = resolve_ip(domain, dns_server, force_ipv4=False)
+            # 如果使用 -6 参数，只使用IPv6进行DNS查询
+            ip = resolve_ip(domain, force_ipv4=False)
         else:
+            # 否则，根据系统的网络配置来选择DNS查询方式
             try:
-                # 尝试使用 IPv4 进行解析
-                ip = resolve_ip(domain, dns_server, force_ipv4=True)
+                # 尝试使用IPv4进行DNS查询
+                ip = resolve_ip(domain, force_ipv4=True)
             except ValueError:
-                # 如果 IPv4 解析失败，尝试使用 IPv6
-                ip = resolve_ip(domain, dns_server, force_ipv4=False)
+                # 如果IPv4查询失败，则使用IPv6进行DNS查询
+                ip = resolve_ip(domain, force_ipv4=False)
 
-        if continuous_ping:
-            print(f"\n正在 TCPing {domain}:{port} [{ip}:{port}] 具有 32 字节的数据:")
-            received_count = 0
-            response_times = []
-            try:
-                while True:  # 无限循环发送请求
-                    if request_nums > 0 and received_count >= request_nums:
-                        break
+        print(f"\n正在 TCPing {domain}:{port} [{ip}:{port}] 具有 32 字节的数据:")
+        received_count = 0
+        response_times = []
+        total_sent = 0
 
-                    start_time = time.time()
-                    try:
-                        with socket.create_connection((ip, port), timeout=timeout / 1000) as conn:
-                            end_time = time.time()
-                            response_time = (end_time - start_time) * 1000  # 转换为毫秒
-                            received_count += 1
-                            response_times.append(response_time)
-                            print(f"来自 {ip}:{port} 的回复: 字节=32 时间={response_time:.0f}ms TTL=64")
-                            if request_nums > 0 and received_count >= request_nums:
-                                break  # 如果达到请求的次数，退出循环
-                            time.sleep(1)  # 每次请求之间添加适当的延迟
+        try:
+            while total_sent < request_nums:
+                if ctrl_c_used:  # Check if Ctrl+C was used
+                    break
 
-                    except socket.timeout:
-                        print("请求超时。")
-                        response_times.append(0)  # 数据包丢失，添加一个占位符的响应时间
-                        if request_nums > 0 and received_count >= request_nums:
-                            break  # 如果达到请求的次数，退出循环
-                        time.sleep(1)  # 每次请求之间添加适当的延迟
-
-                    except (OSError, ConnectionRefusedError) as e:
-                        if isinstance(e, OSError) and e.errno == 10049:
-                            print("请求超时。")
-                            response_times.append(0)  # 数据包丢失，添加一个占位符的响应时间
-                            if request_nums > 0 and received_count >= request_nums:
-                                break  # 如果达到请求的次数，退出循环
-                            time.sleep(1)  # 每次请求之间添加适当的延迟
-                        else:
-                            print(f"无法连接到 {ip}:{port}。")
-                            response_times.append(0)  # 数据包丢失，添加一个占位符的响应时间
-                            if request_nums > 0 and received_count >= request_nums:
-                                break  # 如果达到请求的次数，退出循环
-                            time.sleep(1)  # 每次请求之间添加适当的延迟
-
-            except KeyboardInterrupt:
-                pass
-
-            packet_loss_rate = ((len(response_times) - received_count) / len(response_times)) * 100 if len(
-                response_times) > 0 else 0.0
-            avg_delay = sum(response_times) / received_count if received_count > 0 else 0.0
-            min_delay = min(time_ms for time_ms in response_times if time_ms > 0) if received_count > 0 else 0.0
-            max_delay = max(time_ms for time_ms in response_times if time_ms > 0) if received_count > 0 else 0.0
-
-            print(f"\n{ip}:{port} 的 TCPing 统计信息:")
-            print(
-                f"    数据包: 已发送 = {len(response_times)}, 已接收 = {received_count}，丢失 = {int(len(response_times) - received_count)} ({packet_loss_rate:.1f}% 丢失)")
-
-            if received_count > 0:
-                print("往返行程的估计时间(以毫秒为单位):")
-                print(f"    最短 = {min_delay:.0f}ms，最长 = {max_delay:.0f}ms，平均 = {avg_delay:.0f}ms")
-            else:
-                print("请求全部超时，无法计算往返行程时间.")
-
-        else:
-            print(f"\n正在 TCPing {domain}:{port} [{ip}:{port}] 具有 32 字节的数据:")
-            received_count = 0
-            response_times = []
-            total_sent = 0  # Initialize total_sent to track the actual number of packets sent
-            try:
-                while total_sent < request_nums:
-                    start_time = time.time()
-                    try:
-                        with socket.create_connection((ip, port), timeout=timeout / 1000) as conn:
-                            end_time = time.time()
-                            response_time = (end_time - start_time) * 1000  # Convert to milliseconds
-                            received_count += 1
-                            response_times.append(response_time)
-                            print(f"来自 {ip}:{port} 的回复: 字节=32 时间={response_time:.0f}ms TTL=64")
-                            total_sent += 1  # Increment total_sent only when a packet is sent
-                            time.sleep(1)
-                    except socket.timeout:
+                start_time = time.time()
+                try:
+                    with socket.create_connection((ip, port), timeout=timeout / 1000) as conn:
+                        end_time = time.time()
+                        response_time = (end_time - start_time) * 1000
+                        received_count += 1
+                        response_times.append(response_time)
+                        print(f"来自 {ip}:{port} 的回复: 字节=32 时间={response_time:.0f}ms TTL=64")
+                        total_sent += 1
+                        time.sleep(1)
+                except socket.timeout:
+                    print("请求超时。")
+                    time.sleep(1)
+                except (OSError, ConnectionRefusedError) as e:
+                    if isinstance(e, OSError) and e.errno == 10049:
                         print("请求超时。")
                         time.sleep(1)
-                    except (OSError, ConnectionRefusedError) as e:
-                        if isinstance(e, OSError) and e.errno == 10049:
-                            print("请求超时。")
-                            time.sleep(1)
-                        else:
-                            print(f"无法连接到 {ip}:{port}。")
-                            total_sent += 1  # Increment total_sent even if the packet is lost
-                            time.sleep(1)
+                    else:
+                        print(f"无法连接到 {ip}:{port}。")
+                        total_sent += 1
+                        time.sleep(1)
 
-            except KeyboardInterrupt:
-                pass
+        except KeyboardInterrupt:
+            pass
 
-            packet_loss_rate = ((total_sent - received_count) / total_sent) * 100 if total_sent > 0 else 0.0
-            avg_delay = sum(response_times) / received_count if received_count > 0 else 0.0
-            min_delay = min(response_times) if received_count > 0 else 0.0
-            max_delay = max(response_times) if received_count > 0 else 0.0
+        packet_loss_rate = ((total_sent - received_count) / total_sent) * 100 if total_sent > 0 else 0.0
+        avg_delay = sum(response_times) / received_count if received_count > 0 else 0.0
+        min_delay = min(response_times) if received_count > 0 else 0.0
+        max_delay = max(response_times) if received_count > 0 else 0.0
 
-            print(f"\n{ip}:{port} 的 TCPing 统计信息:")
-            print(
-                f"    数据包: 已发送 = {total_sent}, 已接收 = {received_count}，丢失 = {int(total_sent - received_count)} ({packet_loss_rate:.1f}% 丢失)")
+        print(f"\n{ip}:{port} 的 TCPing 统计信息:")
+        print(f"    数据包: 已发送 = {total_sent}, 已接收 = {received_count}，丢失 = {int(total_sent - received_count)} ({packet_loss_rate:.1f}% 丢失)")
 
-            if received_count > 0:
-                print("往返行程的估计时间(以毫秒为单位):")
-                print(f"    最短 = {min_delay:.0f}ms，最长 = {max_delay:.0f}ms，平均 = {avg_delay:.0f}ms")
-            else:
-                print("请求全部超时，无法计算往返行程时间.")
+        if received_count > 0:
+            print("往返行程的估计时间(以毫秒为单位):")
+            print(f"    最短 = {min_delay:.0f}ms，最长 = {max_delay:.0f}ms，平均 = {avg_delay:.0f}ms")
+        else:
+            print("请求全部超时，无法计算往返行程时间.")
+
+        if ctrl_c_used:  # Only print "Control-C" if Ctrl+C was used
+            print("Control-C")
+
+    except ValueError as e:
+        print(e)
+
+def tcping_continuous(domain, port, force_ipv4, force_ipv6, timeout=1000):
+    try:
+        ip = None
+
+        if force_ipv4:
+            # If the -4 option is used, perform DNS query only for IPv4
+            ip = resolve_ip(domain, force_ipv4=True)
+        elif force_ipv6:
+            # If the -6 option is used, perform DNS query only for IPv6
+            ip = resolve_ip(domain, force_ipv4=False)
+        else:
+            # Otherwise, use the system's network configuration to determine DNS query method
+            try:
+                # Try DNS query for IPv4
+                ip = resolve_ip(domain, force_ipv4=True)
+            except ValueError:
+                # If IPv4 query fails, use IPv6 for DNS query
+                ip = resolve_ip(domain, force_ipv4=False)
+
+        print(f"\n正在 TCPing {domain}:{port} [{ip}:{port}] 具有 32 字节的数据:")
+        request_num = 1
+        response_times = []
+        received_count = 0
+        lost_count = 0
+
+        try:
+            while True:
+                start_time = time.time()
+                try:
+                    with socket.create_connection((ip, port), timeout=timeout / 1000) as conn:
+                        end_time = time.time()
+                        response_time = (end_time - start_time) * 1000  # Convert to milliseconds
+                        response_times.append(response_time)
+                        print(f"来自 {ip}:{port} 的回复: 字节=32 时间={response_time:.0f}ms TTL=64")
+                        received_count += 1
+                        request_num += 1
+                        time.sleep(1)
+                except socket.timeout:
+                    print("请求超时。")
+                    lost_count += 1
+                    request_num += 1
+                    time.sleep(1)
+                except (OSError, ConnectionRefusedError) as e:
+                    if isinstance(e, OSError) and e.errno == 10049:
+                        print("请求超时。")
+                        lost_count += 1
+                        request_num += 1
+                        time.sleep(1)
+                    else:
+                        print(f"无法连接到 {ip}:{port}。")
+                        lost_count += 1
+                        request_num += 1
+                        time.sleep(1)
+        except KeyboardInterrupt:
+            pass
+
+        packet_loss_rate = (lost_count / request_num) * 100 if request_num > 0 else 0.0
+        avg_delay = sum(response_times) / received_count if received_count > 0 else 0.0
+        min_delay = min(response_times) if received_count > 0 else 0.0
+        max_delay = max(response_times) if received_count > 0 else 0.0
+
+        print(f"\n{ip}:{port} 的 TCPing 统计信息:")
+        print(f"    数据包: 已发送 = {request_num - 1}, 已接收 = {received_count}，丢失 = {lost_count} ({packet_loss_rate:.1f}% 丢失)")
+
+        if received_count > 0:
+            print("往返行程的估计时间(以毫秒为单位):")
+            print(f"    最短 = {min_delay:.0f}ms，最长 = {max_delay:.0f}ms，平均 = {avg_delay:.0f}ms")
+        else:
+            print("请求全部超时，无法计算往返行程时间.")
 
     except ValueError as e:
         print(e)
 
 def print_help():
     print("""
-用法: tcpingoo [-t] [-a] [-n count] [-l size] [-f] [-i TTL] [-v TOS]
-            [-r count] [-s count] [[-j host-list] | [-k host-list]]
-            [-w timeout] [-R] [-S srcaddr] [-c compartment] [-p]
-            [-4] [-6] [-d] target_name [port]
+用法: tcpingoo [-n count] [-d DNS_server] [-w timeout] [-4] [-6] target_name port
 
 选项:
-    -t             TCPing 指定的主机，直到停止。
-                   若要查看统计信息并继续操作，请键入 Ctrl+Break；
-                   若要停止，请键入 Ctrl+C。
-    -a             将地址解析为主机名。
-    -l size        发送缓冲区大小。
-    -f             在数据包中设置“不分段”标记(仅适用于 IPv4)。
-    -i TTL         生存时间。
-    -v TOS         服务类型(仅适用于 IPv4。该设置已被弃用，
-                   对 IP 标头中的服务类型字段没有任何
-                   影响)。
-    -r count       记录计数跃点的路由(仅适用于 IPv4)。
-    -s count       计数跃点的时间戳(仅适用于 IPv4)。
-    -j host-list   与主机列表一起使用的松散源路由(仅适用于 IPv4)。
-    -k host-list   与主机列表一起使用的严格源路由(仅适用于 IPv4)。
-    #-w timeout     等待每次回复的超时时间(毫秒)。
-    -R             同样使用路由标头测试反向路由(仅适用于 IPv6)。
-                   根据 RFC 5095，已弃用此路由标头。
-                   如果使用此标头，某些系统可能丢弃
-                   回显请求。
-    -S srcaddr     要使用的源地址。
-    -c compartment 路由隔离舱标识符。
-    -p             Ping Hyper-V 网络虚拟化提供程序地址。
-    #-4             强制使用 IPv4。
-    #-6             强制使用 IPv6。
-    #-d             自定义 DNS 服务器地址
-    """)
+    -n count       计划发送的请求数 (默认: 4)。
+    -d DNS_server  自定义 DNS 服务器地址。
+    -w timeout     每次请求的超时时间(毫秒) (默认: 1000)。
+    -4             强制使用 IPv4 进行查询。
+    -6             强制使用 IPv6 进行查询。
+
+示例:
+    python tcpingoo.b.py yohoky.com 80
+    python tcpingoo.b.py yohoky.com 80 -d 1.1.1.1
+    python tcpingoo.b.py yohoky.com 80 -n 10 -w 500
+    python tcpingoo.b.py yohoky.com 80 -4
+    python tcpingoo.b.py yohoky.com 80 -6
+""")
+    sys.exit(0)
 
 def main():
-    # 使用argparse模块处理命令行参数
-    parser = argparse.ArgumentParser(description="TCPing - Test TCP connectivity to a host and port.")
-    parser.add_argument("domain", type=str, help="Target domain or IP address to TCPing.")
-    parser.add_argument("port", type=int, help="Target port number to TCPing.")
-    parser.add_argument("-n", dest="request_nums", type=int, default=4, help="Number of requests to send (default: 4).")
-    parser.add_argument("-d", dest="dns_server", type=str, help="Custom DNS server address for resolution.")
-    parser.add_argument("-w", dest="timeout", type=int, default=1000, help="Timeout for each request in milliseconds (default: 1000).")
+    parser = argparse.ArgumentParser(description="TCPing 工具 - 使用 TCP 协议检查目标主机端口的可达性。",
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog="示例:\n"
+                                            "python tcpingoo.b.py yohoky.com 80\n"
+                                            "python tcpingoo.b.py yohoky.com 80 -d 1.1.1.1\n"
+                                            "python tcpingoo.b.py yohoky.com 80 -n 10 -w 500\n"
+                                            "python tcpingoo.b.py yohoky.com 80 -4\n"
+                                            "python tcpingoo.b.py yohoky.com 80 -6")
 
-    # 通过添加互斥组，确保 -4 和 -6 参数互斥
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-4", dest="force_ipv4", action="store_true", help="Force using IPv4.")
-    group.add_argument("-6", dest="force_ipv6", action="store_true", help="Force using IPv6.")
+    parser.add_argument("domain", help="要 TCPing 的目标主机名。")
+    parser.add_argument("port", type=int, help="目标主机的端口号。")
+    parser.add_argument("-n", dest="request_nums", metavar="count", type=int, default=4, help="计划发送的请求数 (默认: 4)。")
+    parser.add_argument("-d", dest="dns_server", metavar="DNS_server", default=None, help="自定义 DNS 服务器地址。")
+    parser.add_argument("-w", dest="timeout", metavar="timeout", type=int, default=1000, help="每次请求的超时时间(毫秒) (默认: 1000)。")
+    parser.add_argument("-4", dest="force_ipv4", action="store_true", help="强制使用 IPv4 进行查询。")
+    parser.add_argument("-6", dest="force_ipv6", action="store_true", help="强制使用 IPv6 进行查询。")
+    parser.add_argument("-t", dest="continuous_ping", action="store_true", help="无限执行TCPing直到手动Ctrl+C终止。")
 
-    # 添加 -t 参数来支持连续 TCPing
-    parser.add_argument("-t", dest="continuous_ping", action="store_true", help="TCPing the specified host continuously until stopped.")
+    if len(sys.argv) == 1:
+        print_help()
 
     args = parser.parse_args()
 
     try:
-        if not args.continuous_ping and args.request_nums < 1:
-            args.request_nums = 4  # Set default value to 4 if no value is specified
+        if args.request_nums < 1:
+            args.request_nums = 4
 
-        tcping(args.domain, args.port, args.request_nums, args.force_ipv4, args.force_ipv6, args.dns_server, args.timeout, args.continuous_ping)
+        if args.continuous_ping:
+            tcping_continuous(args.domain, args.port, args.force_ipv4, args.force_ipv6, args.timeout)
+        else:
+            tcping(args.domain, args.port, args.request_nums, args.force_ipv4, args.force_ipv6, args.timeout)
     except ValueError as e:
         print(e)
 
 if __name__ == '__main__':
+    # Set up the signal handler for SIGINT (Ctrl+C)
+    signal.signal(signal.SIGINT, signal_handler)
     main()
