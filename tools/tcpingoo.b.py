7559ee23e211c9d00a5d915056b8b953a7f2f7b2
WeddedMercury15 <wocaowuqing@yandex.com>
2023-07-29 21:31:26 +0800

    Fix

--- tools/tcpingoo.b.py
+++ tools/tcpingoo.b.py
@@ -13,16 +13,10 @@ def custom_gaierror(msg):
 def resolve_ip(hostname, dns_server=None, force_ipv4=False):
     try:
         if dns_server:
-            # Create a custom resolver with the specified DNS server
-            resolver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            resolver.settimeout(1)
-
-            # Use the custom resolver to get address info
+            # Use the custom DNS server for resolution
             family = socket.AF_INET6 if not force_ipv4 else socket.AF_INET
-            addr_info = resolver.getaddrinfo(hostname, None, family)
+            addr_info = socket.getaddrinfo(hostname, None, family, socket.SOCK_STREAM, 0, socket.AI_ADDRCONFIG, None, (dns_server, 53))
             ip = addr_info[0][4][0]  # Get the IP address
-
-            resolver.close()
         else:
             # If no custom DNS server is provided, use the system's default resolver
             family = socket.AF_INET6 if not force_ipv4 else socket.AF_INET
