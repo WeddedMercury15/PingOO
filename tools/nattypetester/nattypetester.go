115e9ee0f603f8abb2c16d328054cf19d6739040
WeddedMercury15 <wocaowuqing@yandex.com>
2023-08-04 19:15:22 +0800

    Update

--- tools/nattypetester/nattypetester.go
+++ tools/nattypetester/nattypetester.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"text/tabwriter"
 
 	"github.com/ccding/go-stun/stun"
 )
@@ -20,28 +22,38 @@ func main() {
 	// 进行STUN测试
 	nat, host, err := client.Discover()
 	if err != nil {
-		fmt.Println(err)
-		return
+		fmt.Fprintf(os.Stderr, "STUN测试失败: %v\n", err)
+		os.Exit(1)
 	}
 
-	// 输出测试结果
-	fmt.Println("NAT类型:", nat)
-	fmt.Println("外部IP地址族:", host.Family())
-	fmt.Println("外部IP地址:", host.IP())
-	fmt.Println("外部端口:", host.Port())
+	// 创建表格写入器
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
+	defer w.Flush()
 
+	// 打印表头
+	fmt.Fprintln(w, "项目\t结果\t")
+
+	// 打印测试结果
+	fmt.Fprintf(w, "NAT类型\t%s\t\n", nat)
+	fmt.Fprintf(w, "外部IP地址族\t%s\t\n", host.Family())
+	fmt.Fprintf(w, "外部IP地址\t%s\t\n", host.IP())
+	fmt.Fprintf(w, "外部端口\t%d\t\n", host.Port())
+
+	// 打印NAT类型描述
+	natDesc := ""
 	switch nat {
 	case stun.NATNone:
-		fmt.Println("未检测到NAT")
+		natDesc = "未检测到NAT"
 	case stun.NATFull:
-		fmt.Println("完全圆锥型NAT")
+		natDesc = "完全圆锥型NAT"
 	case stun.NATSymmetric:
-		fmt.Println("对称型NAT")
+		natDesc = "对称型NAT"
 	case stun.NATRestricted:
-		fmt.Println("地址限制圆锥型NAT")
+		natDesc = "地址限制圆锥型NAT"
 	case stun.NATPortRestricted:
-		fmt.Println("端口限制圆锥型NAT")
+		natDesc = "端口限制圆锥型NAT"
 	default:
-		fmt.Println("未知NAT类型")
+		natDesc = "未知NAT类型"
 	}
+	fmt.Fprintf(w, "\x1b[32m%s\x1b[0m\t\x1b[32m%s\x1b[0m\t\n", "NAT类型描述", natDesc)
 }
