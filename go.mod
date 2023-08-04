af58e74f453af89e3adc0f3f5a5fcbe69050a790
WeddedMercury15 <wocaowuqing@yandex.com>
2023-08-04 23:36:15 +0800

    Update

--- go.mod
+++ go.mod
@@ -8,18 +8,13 @@ require (
 )
 
 require (
+	github.com/jroimartin/gocui v0.5.0 // indirect
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
-	github.com/nsf/termbox-go v0.0.0-20190121233118-02980233997d // indirect
-	golang.org/x/crypto v0.11.0 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
 )
 
 require (
-	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
 	github.com/gizak/termui/v3 v3.1.0
-	github.com/inancgumus/screen v0.0.0-20190314163918-06e984b86ed3
-	github.com/mattn/go-runewidth v0.0.14 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/showwin/speedtest-go v1.6.3
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/term v0.10.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
 )
--- main.go
+++ main.go
@@ -2,66 +2,69 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
 
-	ui "github.com/gizak/termui/v3"
-	"github.com/gizak/termui/v3/widgets"
+	"github.com/jroimartin/gocui"
 )
 
 func main() {
-	if err := ui.Init(); err != nil {
-		fmt.Printf("failed to initialize termui: %v", err)
-		os.Exit(1)
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Fatalf("failed to initialize gocui: %v", err)
 	}
-	defer ui.Close()
+	defer g.Close()
 
-	// 创建一个全屏的布局
-	grid := ui.NewGrid()
-	grid.SetRect(0, 0, 100, 40)
+	g.SetManagerFunc(layout)
 
-	// 创建一个标题
-	title := widgets.NewParagraph()
-	title.Text = "PingOO"
-	title.SetRect(0, 0, 100, 5)
-	title.TextStyle.Fg = ui.ColorYellow
-	title.Border = false
-	title.TextStyle.Modifier = ui.ModifierBold
+	if err := keybindings(g); err != nil {
+		log.Fatalf("failed to set keybindings: %v", err)
+	}
 
-	// 创建三个按钮
-	btn1 := widgets.NewButton("Button 1")
-	btn1.SetRect(25, 10, 45, 13)
-	btn1.TextStyle.Fg = ui.ColorWhite
-	btn1.BorderStyle.Fg = ui.ColorCyan
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Fatalf("gocui main loop error: %v", err)
+	}
+}
 
-	btn2 := widgets.NewButton("Button 2")
-	btn2.SetRect(25, 15, 45, 18)
-	btn2.TextStyle.Fg = ui.ColorWhite
-	btn2.BorderStyle.Fg = ui.ColorCyan
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
 
-	btn3 := widgets.NewButton("Button 3")
-	btn3.SetRect(25, 20, 45, 23)
-	btn3.TextStyle.Fg = ui.ColorWhite
-	btn3.BorderStyle.Fg = ui.ColorCyan
+	// Create a new view for the title
+	if v, err := g.SetView("title", maxX/2-10, 0, maxX/2+10, 3); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = "PingOO"
+		v.Wrap = true
+	}
 
-	// 添加组件到布局
-	grid.Set(
-		ui.NewRow(1.0/6, title),
-		ui.NewRow(1.0/6, widgets.NewSpacer()),
-		ui.NewRow(1.0/6, btn1),
-		ui.NewRow(1.0/6, widgets.NewSpacer()),
-		ui.NewRow(1.0/6, btn2),
-		ui.NewRow(1.0/6, widgets.NewSpacer()),
-		ui.NewRow(1.0/6, btn3),
-	)
-
-	ui.Render(grid)
-
-	// 监听终端输入
-	uiEvents := ui.PollEvents()
-	for {
-		e := <-uiEvents
-		if e.Type == ui.KeyboardEvent && e.ID == "q" {
-			break
+	// Create buttons
+	buttons := []string{"Button 1", "Button 2", "Button 3"}
+	for i, btn := range buttons {
+		if v, err := g.SetView(fmt.Sprintf("btn%d", i+1), maxX/2-10, (i+1)*4, maxX/2+10, (i+1)*4+3); err != nil {
+			if err != gocui.ErrUnknownView {
+				return err
+			}
+			fmt.Fprintln(v, btn)
+			v.Highlight = true
+			v.Frame = true
 		}
 	}
+
+	return nil
+}
+
+func keybindings(g *gocui.Gui) error {
+	// Quit the application when pressing "q" or "Ctrl+C"
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("", 'q', gocui.ModNone, quit); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
