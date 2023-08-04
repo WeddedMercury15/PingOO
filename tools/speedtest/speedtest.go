f6e9cce7fbed4cde7e91135e8a178bacdfbc0bc6
WeddedMercury15 <wocaowuqing@yandex.com>
2023-08-04 21:27:51 +0800

    Update

--- tools/speedtest/speedtest.go
+++ tools/speedtest/speedtest.go
@@ -1,68 +1,107 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"sort"
-	"time"
+	"log"
+	"math"
+
+	"github.com/showwin/speedtest-go/speedtest"
 )
 
-type Servers struct {
-	Servers []Server `xml:"servers>server"`
-}
-
 type Server struct {
-	URL  string  `xml:"url,attr"`
-	Lat  float64 `xml:"lat,attr"`
-	Lon  float64 `xml:"lon,attr"`
-	Name string  `xml:"name,attr"`
+	URL  string  `json:"url"`
+	Name string  `json:"name"`
+	Ping float64 `json:"ping"`
 }
-
-type ByDistance []Server
-
-func (a ByDistance) Len() int           { return len(a) }
-func (a ByDistance) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ByDistance) Less(i, j int) bool { return a[i].Lat < a[j].Lat }
 
 func main() {
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", "https://www.speedtest.net/speedtest-servers-static.php", nil)
-	resp, _ := client.Do(req)
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("正在获取服务器列表...")
+	servers, err := getServerList()
+	if err != nil {
+		log.Fatal("无法获取服务器列表:", err)
+	}
 
-	var s Servers
-	xml.Unmarshal(body, &s)
+	fmt.Println("正在选择最优服务器...")
+	fastestServer := chooseFastestServer(servers)
+	if fastestServer == nil {
+		fmt.Println("无法找到可用服务器")
+		return
+	}
 
-	sort.Sort(ByDistance(s.Servers))
+	fmt.Println("最优服务器信息:")
+	fmt.Printf("名称: %s\n", fastestServer.Name)
+	fmt.Printf("URL: %s\n", fastestServer.URL)
+	fmt.Printf("延迟: %.2f ms\n", fastestServer.Ping)
 
-	testServer := s.Servers[0]
+	fmt.Println("开始测速...")
+	downloadSpeed, uploadSpeed, err := runSpeedTest(fastestServer.URL)
+	if err != nil {
+		log.Fatal("测速失败:", err)
+	}
 
-	fmt.Println("Testing against server:", testServer.Name)
+	fmt.Printf("下载速度: %.2f Mbps\n", downloadSpeed)
+	fmt.Printf("上传速度: %.2f Mbps\n", uploadSpeed)
+}
 
-	start := time.Now()
-	req, _ = http.NewRequest("GET", testServer.URL, nil)
-	resp, _ = client.Do(req)
-	defer resp.Body.Close()
-	body, _ = ioutil.ReadAll(resp.Body)
-	elapsed := time.Since(start)
+func getServerList() ([]Server, error) {
+	client, err := speedtest.NewDefaultClient()
+	if err != nil {
+		return nil, err
+	}
 
-	size := float64(len(body))
-	speed := size / elapsed.Seconds() / 1024 / 1024 * 8
+	// 获取服务器列表
+	servers, err := client.GetServers()
+	if err != nil {
+		return nil, err
+	}
 
-	fmt.Printf("Download speed: %.2f Mbps\n", speed)
+	var serverList []Server
+	for _, s := range servers {
+		serverList = append(serverList, Server{
+			URL:  s.URL,
+			Name: s.Name,
+			Ping: s.Latency.Seconds() * 1000,
+		})
+	}
 
-	start = time.Now()
-	req, _ = http.NewRequest("POST", testServer.URL, nil)
-	resp, _ = client.Do(req)
-	defer resp.Body.Close()
-	body, _ = ioutil.ReadAll(resp.Body)
-	elapsed = time.Since(start)
+	return serverList, nil
+}
 
-	size = float64(len(body))
-	speed = size / elapsed.Seconds() / 1024 / 1024 * 8
+func chooseFastestServer(servers []Server) *Server {
+	// 根据服务器的ping值排序，选择最小的ping值的服务器
+	var fastestServer *Server
+	minPing := math.MaxFloat64
 
-	fmt.Printf("Upload speed: %.2f Mbps\n", speed)
+	for _, server := range servers {
+		if server.Ping < minPing {
+			minPing = server.Ping
+			fastestServer = &server
+		}
+	}
+
+	return fastestServer
+}
+
+func runSpeedTest(serverURL string) (float64, float64, error) {
+	client, err := speedtest.NewDefaultClient()
+	if err != nil {
+		return 0, 0, err
+	}
+
+	server, err := client.GetServer(serverURL)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	downloadSpeed, err := client.Download(server)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	uploadSpeed, err := client.Upload(server)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return downloadSpeed.MegabitsPerSecond, uploadSpeed.MegabitsPerSecond, nil
 }
