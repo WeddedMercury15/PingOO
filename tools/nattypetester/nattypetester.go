0063ae8e1d3b7dea9a98858ba8aaf809d5c82959
WeddedMercury15 <wocaowuqing@yandex.com>
2023-08-04 19:39:25 +0800

    Update

--- tools/nattypetester/nattypetester.go
+++ tools/nattypetester/nattypetester.go
@@ -27,33 +27,35 @@ func main() {
 	}
 
 	// 创建表格写入器
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
 	defer w.Flush()
 
-	// 打印表头
-	fmt.Fprintln(w, "项目\t结果\t")
-
 	// 打印测试结果
-	fmt.Fprintf(w, "NAT类型\t%s\t\n", nat)
-	fmt.Fprintf(w, "外部IP地址族\t%s\t\n", host.Family())
-	fmt.Fprintf(w, "外部IP地址\t%s\t\n", host.IP())
-	fmt.Fprintf(w, "外部端口\t%d\t\n", host.Port())
+	fmt.Fprintln(w, "项目\t结果")
+	fmt.Fprintf(w, "NAT类型\t%s\n", nat)
+	fmt.Fprintf(w, "外部IP地址族\t%s\n", host.Family())
+	fmt.Fprintf(w, "外部IP地址\t%s\n", host.IP())
+	fmt.Fprintf(w, "外部端口\t%d\n", host.Port())
 
 	// 打印NAT类型描述
-	natDesc := ""
+	natDesc := getNATDescription(nat)
+	fmt.Fprintf(w, "\x1b[32m%s\x1b[0m\t\x1b[32m%s\x1b[0m\n", "NAT类型描述", natDesc)
+}
+
+// getNATDescription 根据NAT类型返回相应的描述信息
+func getNATDescription(nat stun.NATType) string {
 	switch nat {
 	case stun.NATNone:
-		natDesc = "未检测到NAT"
+		return "未检测到NAT"
 	case stun.NATFull:
-		natDesc = "完全圆锥型NAT"
+		return "完全圆锥型NAT"
 	case stun.NATSymmetric:
-		natDesc = "对称型NAT"
+		return "对称型NAT"
 	case stun.NATRestricted:
-		natDesc = "地址限制圆锥型NAT"
+		return "地址限制圆锥型NAT"
 	case stun.NATPortRestricted:
-		natDesc = "端口限制圆锥型NAT"
+		return "端口限制圆锥型NAT"
 	default:
-		natDesc = "未知NAT类型"
+		return "未知NAT类型"
 	}
-	fmt.Fprintf(w, "\x1b[32m%s\x1b[0m\t\x1b[32m%s\x1b[0m\t\n", "NAT类型描述", natDesc)
 }
